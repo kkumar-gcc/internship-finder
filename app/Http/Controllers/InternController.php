f4980d513cf97912c49c97016acfb4749639f752
kkumar-gcc <krishkumar9352@gmail.com>
2022-01-04 20:29:07 +0530

    tables added

---
+++ app/Http/Controllers/InternController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Intern;
+use App\Http\Requests\StoreInternRequest;
+use App\Http\Requests\UpdateInternRequest;
+
+class InternController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreInternRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreInternRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Intern  $intern
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Intern $intern)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Intern  $intern
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Intern $intern)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateInternRequest  $request
+     * @param  \App\Models\Intern  $intern
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateInternRequest $request, Intern $intern)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Intern  $intern
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Intern $intern)
+    {
+        //
+    }
+}
--- database/migrations/2022_01_04_104624_create_organisations_table.php
+++ database/migrations/2022_01_04_104624_create_organisations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOrganisationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organisations', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('usertype');
-            $table->string('profile_image');
-            $table->rememberToken();
+            $table->string('full_name');
+            $table->bigInteger('user_id');
+            $table->bigInteger('address_id');
+            $table->timestamp('verified_at');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('organisations');
     }
 }
